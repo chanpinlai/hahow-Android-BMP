8a737f29bfb247d36684eb3d1219f4e4646314b8
chanpinlai <jakgcc@gmail.com>
2024-03-10 00:29:01 +0800

    第 1 章，第 15 單元 - 對話框元件與匿名類別實務

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,12 +1,15 @@
 package com.tom.bmi;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText edHeight;
     private EditText edWeight;
     private TextView result;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
         edHeight = findViewById(R.id.ed_height);
         edWeight = findViewById(R.id.ed_weight);
         result = findViewById(R.id.result);
+        help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("HELP")
+                        .setMessage("ody mass index (BMI) is a value derived from the mass (weight) and height of a person. ")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
     }
 
     public void bmi(View view) {
@@ -37,6 +52,18 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity", "BMI:" + bmi);
         Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
         result.setText("Your BMI is " + bmi);
+        new AlertDialog.Builder(this)
+                .setTitle("BMI")
+                .setMessage("Your BMI is " + bmi)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edHeight.setText("");
+                        edWeight.setText("");
+                    }
+                })
+                .show();
+
 
     }
 }
